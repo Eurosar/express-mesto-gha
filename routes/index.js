4cab7609e9762b68c3bcad6f1b3e5a22dc676c9f
Eurosar <euro.saratov@gmail.com>
2022-07-12 18:27:16 +0300

    Feat: Исправлено обработка неправильного пути код 404

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const cardRouter = require('./cards');
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
 router.use('/', (req, res) => {
-  res.status(400).send({ message: 'Адреса не существует' });
+  res.status(404).send({ message: 'Адреса не существует' });
 });
 
 module.exports = router;
