0ee6106062313ef5f5ca8867a5ff5f1e7e50a97f
Eurosar <euro.saratov@gmail.com>
2022-07-29 20:47:58 +0300

    Feat: Сделана ПР14 для тестов(Попытка 8)

--- controllers/users.js
+++ controllers/users.js
@@ -229,7 +229,7 @@ module.exports.login = (req, res, next) => {
 
       // Отправим токен клиенту и браузер сохранит его в куках
       res
-        .send({ message: 'Вы успешно авторизованы' })
+        .send({ token })
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
