011506c4d1de936f6759c2764b660b4e3c654cf1
Eurosar <euro.saratov@gmail.com>
2022-07-29 14:31:53 +0300

    Feat: Сделана ПР14 для тестов(попытка пройти)

--- validators/celebrate.js
+++ validators/celebrate.js
@@ -4,7 +4,7 @@ module.exports.createUserValidator = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
+    avatar: Joi.string().uri(),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
